416b9ed6b34d4009d918a7ae2a2ccf29d8ffef5e
Viljams Truksans <vilijams25@gmail.com>
2023-03-16 17:09:58 +0200

    Visi galvenie un sub-paneļi ir uztaisīti

--- src/lvt/Aparats.java
+++ src/lvt/Aparats.java
@@ -16,16 +16,49 @@ public class Aparats {
 		JPanel panel=new JPanel();  
         panel.setBounds(0,0,200,500);    
         panel.setBackground(Color.lightGray);
-        panel.setLayout(null); 
+        panel.setLayout(null);
+        panel.add(grozaSubPanel1()); panel.add(grozaSubPanel2());
 		return panel;
 	}
 	public static JPanel pasutijumaPanelis() {
 		JPanel panel=new JPanel();  
         panel.setBounds(200,0,500,500);    
         panel.setBackground(Color.gray);
+        panel.setLayout(null);
+        panel.add(pasutijumasChoosePanel());
+        panel.add(pasutijumasPicaPanel());
+		return panel;
+	}
+	public static JPanel grozaSubPanel1() {
+		JPanel panel=new JPanel();  
+        panel.setBounds(0,0,200,100);    
+        panel.setBackground(Color.yellow);
         panel.setLayout(null); 
 		return panel;
 	}
+	public static JPanel grozaSubPanel2() {
+		JPanel panel=new JPanel();  
+        panel.setBounds(0,10,200,500);    
+        panel.setBackground(Color.pink);
+        panel.setLayout(null); 
+		return panel;
+	}
+	public static JPanel pasutijumasChoosePanel() {
+		JPanel panel=new JPanel();  
+        panel.setBounds(0,0,500,50);    
+        panel.setBackground(Color.cyan);
+        panel.setLayout(null); 
+		return panel;
+	}
+	public static JPanel pasutijumasPicaPanel() {
+		JPanel panel=new JPanel();  
+        panel.setBounds(0,50,500,450);    
+        panel.setBackground(Color.green);
+        panel.setLayout(null); 
+		return panel;
+	}
+	
+	
 	
 	
 	
