43d92db88931ebc232f90f6370eaaed7f0731cd0
Viljams Truksans <vilijams25@gmail.com>
2023-03-16 16:54:25 +0200

    Ielikti 2 galvenie conteineri freimā

--- src/lvt/Aparats.java
+++ src/lvt/Aparats.java
@@ -1,15 +1,32 @@
 package lvt;
 import javax.swing.*;
+import java.awt.*;
 
 public class Aparats {
 
 	public static void myFrame() {
-		JFrame frame = new JFrame("Aparāts");
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(700, 500);
-		
+		 JFrame f= new JFrame("Panel Example");      
+	     f.add(grozaPanelis());
+	     f.add(pasutijumaPanelis());
+	     f.setSize(700,500);    
+	     f.setLayout(null);    
+	     f.setVisible(true);    
 	}
+	public static JPanel grozaPanelis() {
+		JPanel panel=new JPanel();  
+        panel.setBounds(0,0,200,500);    
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(null); 
+		return panel;
+	}
+	public static JPanel pasutijumaPanelis() {
+		JPanel panel=new JPanel();  
+        panel.setBounds(200,0,500,500);    
+        panel.setBackground(Color.gray);
+        panel.setLayout(null); 
+		return panel;
+	}
+	
 	
 	
 	public static void main(String[] args) {
